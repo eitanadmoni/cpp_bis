f97f4b25c0037300594d2dd647ceb202cc8f7847
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-06-17 16:19:34 +0300

    fold

--- Project1/Project1/fold.cpp
+++ Project1/Project1/fold.cpp
@@ -1,4 +1,6 @@
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 
 #define SIZE_OF_BOARD 11
 
@@ -7,13 +9,13 @@ void print_multiplication(int num)
     // function to print the multiplication board from 1 to num
     int i, j = 0;
     std::cout << "Multiplication table up to  " << num << ":" << std::endl;
-    for (i = 1; i <= num; i++)
+    for (i = 1; i <= num; i++) // runing all numbers in the range and print the multiplication
     {
         for (j = 1; j <= num; j++)
         {
-            std::cout << " " << i * j << "  ";
+            std::cout << std::setw(4) << std::left << i * j;
         }
-        std::cout << std::endl;
+        std::cout << "\n"; // new row of the board
     }
 }
 
