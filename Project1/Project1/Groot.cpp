0fb22551223e6d747733f44951a6cab9d7da992d
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-06-17 11:58:12 +0300

    fix comments

--- Project1/Project1/Groot.cpp
+++ Project1/Project1/Groot.cpp
@@ -2,14 +2,22 @@
 
 int main()
 {
-    int num = 0;
+    float num = 0;
     std::cout << "Enter a number: " << std::endl;
     std::cin >> num;
     if (num < 0)
     {
         std::cout << "Num must be greater than 0!";
-        return 0;
+        return -1;
     }
-    std::cout << "sqrt of input is:  " << std::sqrt(num);
+    try
+    {
+        std::cout << "sqrt of input is:  " << std::sqrt(num);
+    }
+    catch (...)
+    {
+        std::cout << "Error while taking sqrt from number, please enter a positive number";
+        return -1;
+    }
     return 0;
 }
