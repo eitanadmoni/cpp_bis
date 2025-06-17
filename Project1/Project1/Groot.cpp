4aa361be2ad068e42dc9cd1c153b220d76d9322b
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-06-17 16:09:34 +0300

    groot

--- Project1/Project1/Groot.cpp
+++ Project1/Project1/Groot.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
 #include <cmath>
-
+#include <iostream>
 
 enum return_values
 {
@@ -8,10 +7,10 @@ enum return_values
     FAILURE = -1,
 };
 
-float print_sqrt(float num) 
+float print_sqrt(float num)
 {
-    //A function to print and return a given number, throwing error if occurs and return 1-
-    
+    // A function to print and return a given number, throwing error if occurs and return 1-
+
     float sqrt = 0;
     if (num < 0) // sqrt can be taken just if num >= 0
     {
@@ -24,10 +23,9 @@ float print_sqrt(float num)
         std::cout << "sqrt of input is:  " << sqrt;
         return sqrt;
     }
-    catch (...) 
+    catch (...)
     {
-        std::cout << "Error while taking sqrt from number, please enter a positive number";
-        return FAILURE;
+        throw "Error while taking sqrt from number, please enter a positive number";
     }
 }
 
@@ -37,6 +35,11 @@ int main()
     float sqrt = 0;
     std::cout << "Enter a number: " << std::endl;
     std::cin >> num; // taking the number to calculate sqrt of from the user
+    if (!std::cin)
+    {
+        std::cout << "Input must be a number!";
+        return FAILURE;
+    }
     sqrt = print_sqrt(num);
     if (sqrt < 0) // if there is some problem in print_sqrt the value will be -1, else sqrt(num) that always >= 0
     {
