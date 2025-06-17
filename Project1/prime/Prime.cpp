59db1cdf3bb4d0bce46205fbcd8b2ce06ff6f343
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-06-17 16:54:21 +0300

    fixing prime

--- Project1/prime/Prime.cpp
+++ Project1/prime/Prime.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include <cmath>
+
 
 #define NUM_TO_CHECK 31
 
 bool is_prime(int num)
 {
+	/*function to check if given integer is prime, the func returns true or false
+	the func iteratng all numbers from 1 to sqrt(num), if the num isn't prime, there is
+	at least 1 number in that range that divides it */
+
 	int i = 0;
 	bool prime = true;
+	float sqrt = 0;
+	sqrt = std::sqrt(num);
 	if (num == 1) {
-		return false;
+		return false; // 1 is not a prime number
 	}
-	for (i = 2; i < num; i++) {
-		if (num % i == 0) {
+	for (i = 2; i <= (int)sqrt; i++) { // looping all the numbers from 2 to sqrt(num) to check primarity
+		if (num % i == 0) { // checking if the current number divide num
 			prime = false;
 		}
 	}
@@ -18,7 +26,7 @@ bool is_prime(int num)
 }
 
 int main() {
-	std::cout << "primary check: " << std::endl;
+	std::cout << "primarity check: " << std::endl;
 	if (is_prime(NUM_TO_CHECK)) {
 		std::cout << NUM_TO_CHECK << " is prime" << std::endl;
 	}
