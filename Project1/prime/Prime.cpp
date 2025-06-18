63e86952186706359974addfc68f8a038dd12853
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-06-18 10:13:58 +0300

    prime

--- Project1/prime/Prime.cpp
+++ Project1/prime/Prime.cpp
@@ -2,36 +2,34 @@
 #include <cmath>
 
 
-#define NUM_TO_CHECK 31
-
-bool is_prime(unsigned int num)
-{
-	/*function to check if given integer is prime, the func returns true or false
+/* function to check if given integer is prime, the func returns true or false
 	the func iteratng all numbers from 1 to sqrt(num), if the num isn't prime, there is
 	at least 1 number in that range that divides it */
 
-	int i = 0;
-	bool prime = true;
-	float sqrt = 0;
-	sqrt = std::sqrt(num);
+bool is_prime(unsigned int num)
+{	
+	using std::sqrt;
+	int num_sqrt = sqrt(num);
 	if (num == 1) {
 		return false; // 1 is not a prime number
 	}
-	for (i = 2; i <= (int)sqrt; i++) { // looping all the numbers from 2 to sqrt(num) to check primarity
+	for (int i = 2; i <= num_sqrt; i++) { // looping all the numbers from 2 to sqrt(num) to check primarity
 		if (num % i == 0) { // checking if the current number divide num
-			prime = false;
+			return false;
 		}
 	}
-	return prime;
+	return true;
 }
 
 int main() {
-	std::cout << "primarity check: " << std::endl;
-	if (is_prime(NUM_TO_CHECK)) {
-		std::cout << NUM_TO_CHECK << " is prime" << std::endl;
+	using std::cout;
+	int num_to_check = 31;
+	cout << "primarity check: " << "\n";
+	if (is_prime(num_to_check)) {
+		cout << num_to_check << " is prime" << "\n";
 	}
 	else {
-		std::cout << NUM_TO_CHECK << " isn't prime" << std::endl;
+		cout << num_to_check << " isn't prime" << "\n";
 	}
 	return 0;
 }
