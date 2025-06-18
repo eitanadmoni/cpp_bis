b7cd904da7aa0c033fd958c528f84991fe505927
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-06-18 16:45:28 +0300

    save

---
+++ Project1/Complex/Complex.h
@@ -0,0 +1,24 @@
+#pragma once
+class ComplexNum
+{
+public:
+	ComplexNum();
+	ComplexNum(int a, int b);
+	int getReal() const;
+	int getImagionary() const;
+
+	void setReal(int a);
+	void setImagionary(int b);
+
+	ComplexNum operator+(const ComplexNum& other) const;
+	ComplexNum operator-(const ComplexNum& other) const;
+	ComplexNum operator*(const ComplexNum& other) const;
+	ComplexNum operator==(const ComplexNum& other) const;
+
+	void printComplex();
+
+
+private:
+	int m_real;
+	int m_imagionary;
+};
