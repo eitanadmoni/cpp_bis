099b72d3c56aba7d2864d51db6bfeaa594b05289
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2025-06-18 10:39:20 +0300

    groooooot

--- Project1/Project1/Groot.cpp
+++ Project1/Project1/Groot.cpp
@@ -1,8 +1,19 @@
 #include <cmath>
 #include <iostream>
 
+enum error_code
+{
+    NOT_NUM,
+    NEGATIVE,
+    SQRT_ERR,
+};
 
-float print_sqrt(float num)
+enum return_values
+{
+    SUCCESS,
+    FAILURE = -1,
+};
+    float print_sqrt(float num)
 {
     // A function to print and return a given number, throwing error if occurs and return 1-
     float sqrt = 0;
@@ -14,7 +25,7 @@ float print_sqrt(float num)
     }
     catch (...)
     {
-        throw "Error while taking sqrt from number, please enter a positive number";
+        throw SQRT_ERR;
     }
 }
 
@@ -26,11 +37,11 @@ float get_input_num()
     std::cin >> num; // taking the number to calculate sqrt of from the user
     if (!std::cin)
     {
-        throw "Input must be a number!";
+        throw NOT_NUM;
     }
     if (num < 0) // sqrt can be taken just if num >= 0
     {
-        throw "Num must be greater than 0!";
+        throw NEGATIVE;
     }
     return num;
 }
@@ -38,8 +49,30 @@ float get_input_num()
 int main()
 {
     float num = 0;
-
-    num = get_input_num();
-    print_sqrt(num);
-    return 0;
+    using std::cout;
+    try
+    {
+        num = get_input_num();
+        print_sqrt(num);
+    
+    }
+    catch (error_code errorCode)
+    {
+        switch (errorCode)
+        {
+        case NOT_NUM:
+            cout << "Input must be a number!";
+            return FAILURE;
+        case NEGATIVE:
+            cout << "Number must be positive!";
+            return FAILURE;
+        case SQRT_ERR:
+            cout << "Error while taking sqrt";
+            return FAILURE;
+        default:
+            cout << "Undefined error";
+            return FAILURE;
+        }
+    }
+    return SUCCESS;
 }
